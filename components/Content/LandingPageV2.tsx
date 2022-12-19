e822ba615f4c5f6967c0ae738bafebad350da620
Landan <LandanParker@gmail.com>
2022-12-18 20:34:19 -0600

    .

--- components/Content/LandingPageV2.tsx
+++ components/Content/LandingPageV2.tsx
@@ -5,8 +5,10 @@ var titleImageLocation = 'https://cuscasperplatform.blob.core.windows.net/test/s
 
 export function LandingPageV2(){
     var pageImage = (<div className={[
+        styles.fullScreen,
         styles.noRepeatImage,
         styles.imageHeightMax,
+        styles.contain,
         styles.absolute,
         styles.z1
     ].join(' ')} style={{backgroundImage: `url(${titleImageLocation})`}}/>)
@@ -34,17 +36,28 @@ var LandingPageLinks = [
     <Link href={'#about_blank'}>Sign in</Link>,
 ];
 
+var inlineCard = (
+    <div className={[
+        styles.unfullScreen,
+        styles.imageHeightMax,
+        styles.noRepeatImage,
+        styles.cover,
+    ].join(' ')}
+    style={{backgroundImage: `url(${titleImageLocation})`, aspectRatio: 1}}>{
+    }</div>)
+
 function GetText(){
     return (
     <div className={[
         styles.flexRow,
-        styles.justifyEnd,
+        styles.landingPageAlignment,
         styles.pad20,
         styles.scrollFit,
+        styles.maxW1400,
+        styles.z2,
     ].join(' ')}>
         <div className={[
             styles.fontColor,
-            styles.z2,
             styles.bkRed,
             styles.relative,
             styles.preWrap,
@@ -52,16 +65,25 @@ function GetText(){
             styles.paraLine,
             styles.paragraphContainer,
             styles.swipeMode,
-            styles.paragraphItems
+            styles.paragraphItems,
         ].join(' ')}>{
             [
-                `Welcome to Project Casper! We are a community-driven platform that is dedicated to supporting content creators and helping them engage with their viewers. Our goal is to bring new technologies to the streaming ecosystem that will revolutionize the way creators and viewers interact.`,
-                `We believe that everyone has something unique and valuable to share with the world, and our platform is designed to give content creators the tools they need to succeed. Whether you are a seasoned creator looking to take your channel to the next level or a newcomer just starting out, we have something for you.`,
-                `With Project Casper, you can expect a seamless streaming experience, innovative features that enhance viewer engagement, and a supportive community of like-minded creators. Our team is constantly working to improve the platform and add new features, so you can focus on what you do best: creating amazing content.`,
-                `Whether you're a content creator looking to take your stream to the next level, or a viewer looking to support and connect with your favorite creators, Project Casper has something for everyone. So join us today and be a part of the future of streaming.`,
-            ].map((e,i)=>(<div key={i}><div className={styles.blurBk}>{e}</div></div>))
+                GetImageCard(inlineCard),
+                GetTextCard(`Welcome to Project Casper! We are a community-driven platform that is dedicated to supporting content creators and helping them engage with their viewers. Our goal is to bring new technologies to the streaming ecosystem that will revolutionize the way creators and viewers interact.`),
+                GetTextCard(`We believe that everyone has something unique and valuable to share with the world, and our platform is designed to give content creators the tools they need to succeed. Whether you are a seasoned creator looking to take your channel to the next level or a newcomer just starting out, we have something for you.`),
+                GetTextCard(`With Project Casper, you can expect a seamless streaming experience, innovative features that enhance viewer engagement, and a supportive community of like-minded creators. Our team is constantly working to improve the platform and add new features, so you can focus on what you do best: creating amazing content.`),
+                GetTextCard(`Whether you're a content creator looking to take your stream to the next level, or a viewer looking to support and connect with your favorite creators, Project Casper has something for everyone. So join us today and be a part of the future of streaming.`),
+            ].map((e,i)=>(e(i)))
             }</div>
     </div>)
+}
+
+function GetTextCard(text:any){
+    return (a:number) =><div key={a} className={styles.blurBk}>{text}</div>
+}
+
+function GetImageCard(item:any){
+    return (a:number) =>item
 }
 
 function GetCarouselButtons(){
